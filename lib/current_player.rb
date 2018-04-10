1f5f46a2b6b273d4b7e040bdecc7b530451c61f8
Kartik Bhalla <kartikbhalla53@yahoo.com>
2018-04-10 09:39:59 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,21 @@
+def turn_count(board)
+  i = 0
+  count = 0
+  while i < 9 
+    if board[i] != " " || board[i] != "" || board[i] != nil
+      count += 1
+    end
+    i++
+  end
+return count
+end
+
+def current_player(board)
+  count = turn_count(board)
+  if count % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+    
